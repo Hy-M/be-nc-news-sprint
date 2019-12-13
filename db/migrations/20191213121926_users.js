adcffef9c395a46ce50fec452f8bacc4cb15ba20
Hy-M <humayraa.m@outlook.com>
2019-12-13 12:42:27 +0000

    Creating users table with columns

--- db/migrations/20191213121926_users.js
+++ db/migrations/20191213121926_users.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    console.log("creating the users table...");
+    return knex.schema.createTable('users', (usersTable) => {
+        usersTable.string('username', 100).primary().unique().notNullable();
+        usersTable.string('avatar_url', 100);
+        usersTable.string('name', 100).notNullable();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    console.log("dropping the users table...");
+    return knex.schema.dropTable('users');
 };
