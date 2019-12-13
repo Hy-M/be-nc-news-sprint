343b0c2d5b7cb1a4bae0dcd227951e1910dba516
Hy-M <humayraa.m@outlook.com>
2019-12-13 14:09:11 +0000

    Creating articles table with columns

---
+++ db/migrations/20191213124650_articles.js.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  console.log("creating the articles table...");
+  return knex.schema.createTable('articles', (articlesTable) => {
+      articlesTable.increments('article_id').primary();
+      articlesTable.string('title', 100).notNullable();
+      articlesTable.string('body').notNullable();
+      articlesTable.integer('votes').defaultTo(0);
+      articlesTable.string('topic', 100).references('topics.slug').notNullable();
+      articlesTable.string('author', 100).references('users.username').notNullable();
+      articlesTable.timestamp('created_at');
+  })
+};
+
+exports.down = function(knex) {
+    console.log("dropping the articles table...");
+};
