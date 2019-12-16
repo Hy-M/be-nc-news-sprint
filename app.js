b71ed8b2eeea17052d034e6185c7c16dae4ea186
Hy-M <humayraa.m@outlook.com>
2019-12-16 16:27:31 +0000

    Implementing error-handling for first endpoint with TDD

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router.js");
+const { handle404 } = require("./errors/errors");
+
 
 app.use('/api', apiRouter);
+app.use('/*', handle404);
+
 
 module.exports = app;
--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,7 +1,6 @@
 const { fetchTopics } = require("../models/topics-model");
 
 exports.getTopics = (req, res, next) => {
-    console.log("in the controller");
     fetchTopics()
     .then((response) => {
        res.status(200).send(response);  
