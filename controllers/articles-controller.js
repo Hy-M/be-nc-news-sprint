6b400422770117bd6ec7e50738ae69e5c78b8865
Hy-M <humayraa.m@outlook.com>
2019-12-18 11:07:34 +0000

    Implementing error-handling for fourth endpoint and refactored tests

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,5 +1,5 @@
 const {
-    fetchArticleById
+    fetchArticleById, updateArticleById
 } = require("../models/articles-model");
 
 exports.getArticleById = (req, res, next) => {
@@ -8,7 +8,17 @@ exports.getArticleById = (req, res, next) => {
     } = req.params;
     fetchArticleById(article_id)
     .then((response) => {
-       res.status(200).send({article: response});
+       res.status(200).send({article: response[0]});
+    })
+    .catch(next);
+};
+
+exports.patchArticleById = (req, res, next) => {
+    const { inc_votes } = req.body;
+    const { article_id } = req.params;
+    updateArticleById(article_id, inc_votes)
+    .then((response) => {
+        res.status(200).send({article: response[0]})
     })
     .catch(next);
 };
