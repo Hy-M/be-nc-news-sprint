d6ee1df92c40bd89b77ef7b7f4943d51fefffe03
Hy-M <humayraa.m@outlook.com>
2020-02-05 11:28:28 +0000

    Cleaning up code

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api', apiRouter);
-
 app.use('/*', (req, res, next) => {
     res.status(404)
     .send({msg: "Path not found"});
 });
-
 
 app.use(handle404);
 app.use(handle400);
